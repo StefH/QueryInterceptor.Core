90abb3e28e382fdd82296b8813729a63befe6ff7
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-04-21 12:25:51 +0000

    Expose OriginalProvider as property on the QueryTranslatorProvider

--- src/QueryInterceptor.Core/QueryTranslatorProvider.cs
+++ src/QueryInterceptor.Core/QueryTranslatorProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Linq.Expressions;
 using QueryInterceptor.Core.Validation;
+using JetBrains.Annotations;
 
 namespace QueryInterceptor.Core
 {
@@ -10,13 +11,17 @@ namespace QueryInterceptor.Core
         /// Initializes a new instance of the <see cref="QueryTranslatorProvider"/> class.
         /// </summary>
         /// <param name="source">The source.</param>
-        protected QueryTranslatorProvider(IQueryable source)
+        protected QueryTranslatorProvider([NotNull] IQueryable source)
         {
             Check.NotNull(source, nameof(source));
+            Check.NotNull(source.Provider, nameof(source.Provider));
 
             Source = source;
+            OriginalProvider = source.Provider;
         }
 
         internal IQueryable Source { get; }
+
+        public IQueryProvider OriginalProvider { get; private set; }
     }
 }
